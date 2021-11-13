ea0db6ef6a67e0b80add50549b2bdddb8bc93d17
mhd <mhdhd@gmail.com>
2021-11-13 23:30:27 +0400

    working client & server

--- src/Client.java
+++ src/Client.java
@@ -4,57 +4,59 @@ import java.util.Scanner;
 
 public class Client {
 
+    DataOutputStream dataOutput;
+    DataInputStream dataInput;
     Socket socket;
     ObjectInputStream ois;
-    ObjectOutputStream oos;
-    public Client() throws IOException {
+    public Client() {
         try{
 
             socket = new Socket("localhost",34000);
-            ois = new ObjectInputStream(socket.getInputStream());
-            oos = new ObjectOutputStream(socket.getOutputStream());
+            System.out.println("connected!!..");
+            dataOutput = new DataOutputStream(socket.getOutputStream());
+            dataInput = new DataInputStream(socket.getInputStream());
+
         }catch(Exception e){
             System.out.println(e);
         }
     }
 
-    public void sendUsername(String username) throws IOException {
-        oos.writeObject(username);
-        oos.flush();
+    public void sendMessage(String msg) throws IOException {
+        dataOutput.writeUTF(msg);
     }
 
-   public char[][] getOpponentBoard() throws IOException, ClassNotFoundException {
-       return (char[][]) ois.readObject();
-   }
-    public void sendMyBoard(char[][] board) throws IOException {
-        oos.writeObject(board);
-        oos.flush();
+    public void sendUsername(String msg) throws IOException {
+        dataOutput.writeUTF(msg);
     }
-   public String getOpponentName() throws IOException, ClassNotFoundException {
-       return (String) ois.readObject();
-   }
-   public void closeSocket() throws IOException {
-       ois.close();
-       socket.close();
-   }
+
+    public void readFromServer() throws IOException, ClassNotFoundException {
+        ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
+        String message = (String) ois.readObject();
+        System.out.println("Message from server: " + message);
+    }
+
+    public void readFromServerv2() throws IOException, ClassNotFoundException {
+        ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
+        String message = (String) dataInput.readUTF();
+        System.out.println("Message from server: " + message);
+    }
+    public void closeSocket() throws IOException {
+        dataOutput.flush();
+        dataOutput.close();
+        socket.close();
+    }
 
     public static void main(String[] args) throws IOException, ClassNotFoundException {
         Client client  = new Client();
         Grid player2 = new Grid();
         Scanner input = new Scanner(System.in);
 
-//        player2.setOpponentName(client.getOpponentName());
-        System.out.println(player2.getOpponentName());
         System.out.print("set username> ");
         String username = input.next();
+
         client.sendUsername(username);
-        player2.setMyName(username);
-
-//        char[][] opponentBoard = client.getOpponentBoard();
-//        player2.getBothBoards(opponentBoard);
-
-//        client.sendMyBoard(player2.getGameBoardWithHits());
-
+        client.sendUsername(username.substring(4));
+        client.readFromServerv2();
         client.closeSocket();
     }
 }
--- src/Server.java
+++ src/Server.java
@@ -7,35 +7,22 @@ import java.util.Scanner;
 public class Server {
     public static void main(String[] args) {
         try{
-            Grid player1  = new Grid();
             ServerSocket ss = new ServerSocket(34000);
-            Scanner input = new Scanner(System.in);
-
-            System.out.print("set username> ");
-            String username = input.next();
-            player1.setMyName(username);
             System.out.println("waiting for a client...");
-
-
             Socket socket=ss.accept();//establishes connection
+            System.out.println("connected!!..");
             // reading from the client
-            ObjectInputStream dis = new ObjectInputStream(socket.getInputStream());
-
+            DataInputStream dis = new DataInputStream(socket.getInputStream());
+            String  opponentName = (String) dis.readUTF();
+            System.out.printf("%s has been connected",opponentName);
             //create ObjectOutputStream object for sending to client
             ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
+            DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
             //write object to Socket
-            oos.writeObject(player1.getMyName());
-            oos.flush();
-            String  opponentName = (String) dis.readObject();
-            player1.setOpponentName(opponentName);
-            System.out.printf("%s has been connected!\n",player1.getOpponentName());
-            oos.writeObject(player1.getGameBoardWithHits());
-            oos.flush();
-
-            char[][] opponentBoard = (char[][]) dis.readObject();
-            player1.getBothBoards(opponentBoard);
-
-            oos.close();
+//            oos.writeObject("Hi Client "+opponentName.toUpperCase());
+//            oos.close();
+            dos.writeUTF("hi "+ opponentName);
+            dos.flush();
             ss.close();
         }catch(Exception e){
             System.out.println(e);
